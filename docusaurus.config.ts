a480c0b2bda49478708fb2d5e2c8aff334f64a7d
dangattringer <daniel.gattringer100@gmx.at>
2025-04-29 06:27:57 +0200

    refactor: update POSTHOG_API_KEY handling in workflow and Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,7 +2,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-
+const POSTHOG_API_KEY = process.env.POSTHOG_API_KEY || "";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
@@ -44,7 +44,7 @@ const config: Config = {
     [
       "posthog-docusaurus",
       {
-        apiKey: process.env.POSTHOG_API_KEY || "",
+        apiKey: POSTHOG_API_KEY,
         appUrl: "https://eu.i.posthog.com",
         enableInDevelopment: false,
       },
